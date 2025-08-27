5f0889bfd2209ed01f53141cfeec8882b5be4e2c
maryam <Ofogh.maryam14@gmail.com>
2025-08-27 10:57:07 +0330

    fix cart items after refresh in localstorage

--- maxim-resturant/src/redux/cartSlice.js
+++ maxim-resturant/src/redux/cartSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
   name: "cart",
   initialState: [],
+  initialState: JSON.parse(localStorage.getItem("cart")) ?? [],
   reducers: {
     addToCart: (state, action) => {
       const item = action.payload;
@@ -13,18 +14,24 @@ const cartSlice = createSlice({
       } else {
         state.push({ ...item, quantity: 1 });
       }
+      // ذخیره در localStorage
+      localStorage.setItem("cart", JSON.stringify(state));
     },
     removeFromCart: (state, action) => {
       const item = action.payload;
       const existing = state.find((i) => i.id === item.id);
-
-      if (existing.quantity>1) {
+      let newState = state;
+      if (existing.quantity > 1) {
         existing.quantity -= 1;
       } else {
-        return state.filter(i => i.id !== item.id);
+        newState = state.filter((i) => i.id !== item.id);
       }
+      // ذخیره در localStorage
+      localStorage.setItem("cart", JSON.stringify(newState));
+      return newState;
     },
     clearCart: () => {
+      localStorage.removeItem("cart"); // پاک کردن
       return [];
     },
   },
